53f20a19c7dbb9ce0a34b4bfb368fd6473d0ceac
Gaston Mollica <mollica.gaston@gmail.com>
2018-01-11 16:17:49 +1000

    basic commands

--- JavaScriptPractise.js
+++ JavaScriptPractise.js
@@ -1,1 +1,9 @@
-JavaScriptPractise.js
+console.log('Hello')
+console.log('I am pushing stuff to the screen')
+
+console.log(2+2)
+console.log(2018 - 1969)
+
+console.log('Maximiliano Gaston Mollica'.length);
+
+console.log('maximiliano gaston mollica'.toUpperCase())
